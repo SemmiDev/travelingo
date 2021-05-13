2e974b59012d8a8d8965344072270bdd544ccbb8
SemmiDev <sammidev4@gmail.com>
2021-05-13 20:01:39 +0700

    ADD JWT

--- go.mod
+++ go.mod
@@ -3,6 +3,9 @@ module travelapp
 go 1.16
 
 require (
+	github.com/form3tech-oss/jwt-go v3.2.2+incompatible
 	github.com/gofiber/fiber/v2 v2.9.0
+	github.com/gofiber/jwt/v2 v2.2.1
+	github.com/joho/godotenv v1.3.0
 	go.mongodb.org/mongo-driver v1.5.2
 )
--- main.go
+++ main.go
@@ -4,11 +4,12 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"github.com/form3tech-oss/jwt-go"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
+	jwtMiddleware "github.com/gofiber/jwt/v2"
 	"github.com/joho/godotenv"
-	_ "github.com/joho/godotenv/autoload"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -17,6 +18,8 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -36,9 +39,9 @@ func IsProduction() bool {
 // Travel for field represent in table
 type Travel struct {
 	ObjectID primitive.ObjectID `json:"id" bson:"_id"`
-	Name 	string `json:"name" bson:"name"`
-	Photo 	string `json:"photo" bson:"photo"`
-	Done 	bool `json:"done" bson:"done"`
+	Name 	string 	`json:"name" 	bson:"name"`
+	Photo 	string 	`json:"photo" 	bson:"photo"`
+	Done 	bool 	`json:"done" 	bson:"done"`
 }
 
 // Travels for Travel slices
@@ -238,19 +241,57 @@ func (a *appService) getTravel(c *fiber.Ctx) error {
 
 // getTravel() for create a Travel
 func (a *appService) createTravel(c *fiber.Ctx) error {
+	now := time.Now().Unix()
+
+	// Get claims from JWT.
+	claims, err := ExtractTokenMetadata(c)
+	if err != nil {
+		// Return status 500 and JWT parse error.
+		return response(nil, fiber.StatusInternalServerError, err,c)
+	}
+
+	// Set expiration time from JWT data of current product.
+	expires := claims.Expires
+
+	// Checking, if now time greater than expiration from JWT.
+	if now > expires {
+		// Return status 401 and unauthorized error message.
+		msg := "unauthorized, check expiration time of your token"
+		return response(nil, fiber.StatusUnauthorized, errors.New(msg),c)
+	}
+
 	var travel Travel
 	if err := c.BodyParser(&travel); err != nil {
 		return response(travel, http.StatusUnprocessableEntity, err, c)
 	}
-	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 20 * time.Second)
 	defer cancel()
 
-	err := a.Repository.insertOne(ctx, &travel)
+	err = a.Repository.insertOne(ctx, &travel)
 	return response(travel, http.StatusOK, err, c)
 }
 
 // updateTravel() for update a Travel
 func (a *appService) updateTravel(c *fiber.Ctx) error {
+	now := time.Now().Unix()
+
+	// Get claims from JWT.
+	claims, err := ExtractTokenMetadata(c)
+	if err != nil {
+		// Return status 500 and JWT parse error.
+		return response(nil, fiber.StatusInternalServerError, err,c)
+	}
+
+	// Set expiration time from JWT data of current product.
+	expires := claims.Expires
+
+	// Checking, if now time greater than expiration from JWT.
+	if now > expires {
+		// Return status 401 and unauthorized error message.
+		msg := "unauthorized, check expiration time of your token"
+		return response(nil, fiber.StatusUnauthorized, errors.New(msg),c)
+	}
+
 	id := c.Params("id")
 	log.Println(id)
 	if id == "" {
@@ -264,12 +305,31 @@ func (a *appService) updateTravel(c *fiber.Ctx) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
 	defer cancel()
 
-	err := a.Repository.updateOne(ctx, id, &travel)
+	err = a.Repository.updateOne(ctx, id, &travel)
 	return response(nil, http.StatusNoContent, err, c)
 }
 
 // deleteTravel() for delete a travel
 func (a *appService) deleteTravel(c *fiber.Ctx) error {
+	now := time.Now().Unix()
+
+	// Get claims from JWT.
+	claims, err := ExtractTokenMetadata(c)
+	if err != nil {
+		// Return status 500 and JWT parse error.
+		return response(nil, fiber.StatusInternalServerError, err,c)
+	}
+
+	// Set expiration time from JWT data of current product.
+	expires := claims.Expires
+
+	// Checking, if now time greater than expiration from JWT.
+	if now > expires {
+		// Return status 401 and unauthorized error message.
+		msg := "unauthorized, check expiration time of your token"
+		return response(nil, fiber.StatusUnauthorized, errors.New(msg),c)
+	}
+
 	id := c.Params("id")
 	log.Println(id)
 	if id == "" {
@@ -279,7 +339,7 @@ func (a *appService) deleteTravel(c *fiber.Ctx) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
 	defer cancel()
 
-	err := a.Repository.deleteOne(ctx, id)
+	err = a.Repository.deleteOne(ctx, id)
 	return response(nil, http.StatusNoContent, err, c)
 }
 
@@ -302,6 +362,7 @@ func response(data interface{}, httpStatus int, err error, c *fiber.Ctx) error {
 // Routes for endpoint to access handler
 func Routes(app *fiber.App, service Service) {
 	api := app.Group("/api/v1")
+
 	api.Get("/health", func(c *fiber.Ctx) error {
 		return c.Status(http.StatusOK).
 			JSON(map[string]interface{}{
@@ -310,20 +371,150 @@ func Routes(app *fiber.App, service Service) {
 			})
 	})
 
+	// public endpoint
+	api.Get("/token/new", GetNewAccessToken)
 	api.Get("/travels", service.getTravels)
 	api.Get("/travels/:id", service.getTravel)
-	api.Post("/travels", service.createTravel)
-	api.Put("/travels/:id", service.updateTravel)
-	api.Delete("/travels/:id", service.deleteTravel)
+
+	// private endpoint
+	api.Post("/travels", JWTProtected(), service.createTravel)
+	api.Put("/travels/:id", JWTProtected(), service.updateTravel)
+	api.Delete("/travels/:id", JWTProtected(), service.deleteTravel)
 }
 
-// yeah!! GO
-func main() {
-	log.SetFlags(log.LstdFlags | log.Lshortfile)
-	log.Printf("Running application in %v environment", os.Getenv("APP_PROFILE"))
-	if err := run(); err != nil {
-		log.Fatal(err)
+// JWTProtected func for specify routes group with JWT authentication.
+// See: https://github.com/gofiber/jwt
+func JWTProtected() func(*fiber.Ctx) error {
+	// Create config for JWT authentication middleware.
+	config := jwtMiddleware.Config{
+		SigningKey:   []byte(os.Getenv("JWT_SECRET_KEY")),
+		ContextKey:   "jwt", // used in private routes
+		ErrorHandler: jwtError,
 	}
+
+	return jwtMiddleware.New(config)
+}
+
+func jwtError(c *fiber.Ctx, err error) error {
+	// Return status 401 and failed authentication error.
+	if err.Error() == "Missing or malformed JWT" {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": true,
+			"msg":   err.Error(),
+		})
+	}
+
+	// Return status 401 and failed authentication error.
+	return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+		"error": true,
+		"msg":   err.Error(),
+	})
+}
+
+// TokenMetadata struct to describe metadata in JWT.
+type TokenMetadata struct {
+	Expires int64
+}
+
+// ExtractTokenMetadata func to extract metadata from JWT.
+func ExtractTokenMetadata(c *fiber.Ctx) (*TokenMetadata, error) {
+	token, err := verifyToken(c)
+	if err != nil {
+		return nil, err
+	}
+
+	// Setting and checking token and credentials.
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if ok && token.Valid {
+		// Expires time.
+		expires := int64(claims["exp"].(float64))
+
+		return &TokenMetadata{
+			Expires: expires,
+		}, nil
+	}
+
+	return nil, err
+}
+
+func extractToken(c *fiber.Ctx) string {
+	bearToken := c.Get("Authorization")
+
+	// Normally Authorization HTTP header.
+	onlyToken := strings.Split(bearToken, " ")
+	if len(onlyToken) == 2 {
+		return onlyToken[1]
+	}
+
+	return ""
+}
+
+func verifyToken(c *fiber.Ctx) (*jwt.Token, error) {
+	tokenString := extractToken(c)
+
+	token, err := jwt.Parse(tokenString, jwtKeyFunc)
+	if err != nil {
+		return nil, err
+	}
+
+	return token, nil
+}
+
+func jwtKeyFunc(token *jwt.Token) (interface{}, error) {
+	return []byte(os.Getenv("JWT_SECRET_KEY")), nil
+}
+
+// GenerateNewAccessToken func for generate a new Access token.
+func GenerateNewAccessToken() (string, error) {
+	// Set secret key from .env file.
+	secret := os.Getenv("JWT_SECRET_KEY")
+
+	// Set expires minutes count for secret key from .env file.
+	minutesCount, _ := strconv.Atoi(os.Getenv("JWT_SECRET_KEY_EXPIRE_MINUTES_COUNT"))
+
+	// Create a new claims.
+	claims := jwt.MapClaims{}
+
+	// Set public claims:
+	claims["exp"] = time.Now().Add(time.Minute * time.Duration(minutesCount)).Unix()
+
+	// Create a new JWT access token with claims.
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	// Generate token.
+	t, err := token.SignedString([]byte(secret))
+	if err != nil {
+		// Return error, it JWT token generation failed.
+		return "", err
+	}
+
+	return t, nil
+}
+
+// GetNewAccessToken method for create a new access token.
+// @Description Create a new access token.
+// @Summary create a new access token
+// @Tags Token
+// @Accept json
+// @Produce json
+// @Success 200 {string} status "ok"
+// @Router /v1/token/new [get]
+func GetNewAccessToken(c *fiber.Ctx) error {
+	// Generate a new Access token.
+	token, err := GenerateNewAccessToken()
+	if err != nil {
+		// Return status 500 and token generation error.
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": true,
+			"msg":   err.Error(),
+		})
+	}
+
+	return c.JSON(fiber.Map{
+		"error":        false,
+		"msg":          nil,
+		"access_token": token,
+	})
 }
 
 // run() for initialize fiber app
@@ -343,7 +534,11 @@ func run() error {
 	service := NewService(r)
 
 	// fiber initialize
-	app := fiber.New()
+	readTimeoutSecondsCount, _ := strconv.Atoi(os.Getenv("SERVER_READ_TIMEOUT"))
+	app := fiber.New(fiber.Config{
+		ReadTimeout: time.Second * time.Duration(readTimeoutSecondsCount),
+	})
+
 	if !IsProduction() {
 		app.Use(logger.New())
 		app.Use(cors.New())
@@ -352,4 +547,14 @@ func run() error {
 	// service -> routes
 	Routes(app, service)
 	return app.Listen(fmt.Sprintf(":%s", port))
+}
+
+// yeah!! GO
+func main() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	log.Printf("Running application in %v environment", os.Getenv("APP_PROFILE"))
+
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
 }
